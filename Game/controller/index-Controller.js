8f3802d11f32582cd1b43ff24b92379ad3e4b4d8
ureshaL <ureshalakshani967@gmail.com>
2019-07-10 12:42:29 +0530

    my car move created

--- Game/controller/index-Controller.js
+++ Game/controller/index-Controller.js
@@ -12,6 +12,8 @@ const car_2 = $('#car2');
 const car_3 = $('#car3');
 
 const my_car = $('#car');
+const my_car_width = $('#car').width();
+const my_car_height = $('#car').height();
 
 var move_up = false;
 var move_down = false;
@@ -20,6 +22,7 @@ var move_right = false;
 
 let game_over = false;
 let gameHeight = $('.wrapper').height();
+let gameWidth = $('.wrapper').width();
 
 let step = 1;
 
@@ -30,6 +33,12 @@ $(document).on('keydown', function (e) {
         var key = e.keyCode;
         if (key === 37 && move_left === false){
                 move_left = requestAnimationFrame(left);
+        }else if (key === 39 && move_right === false){
+            move_right = requestAnimationFrame(right);
+        }else if (key === 38 && move_up === false){
+            move_up = requestAnimationFrame(up);
+        }else if (key === 40 && move_down === false){
+            move_down = requestAnimationFrame(down);
         }
     }
 });
@@ -40,6 +49,15 @@ $(document).on('keyup', function (e) {
         if (key === 37){
             cancelAnimationFrame(move_left);
             move_left = false;
+        }else if (key === 39){
+            cancelAnimationFrame(move_right);
+            move_right = false;
+        }else if (key === 38){
+            cancelAnimationFrame(move_up);
+            move_up = false;
+        }else if (key === 40){
+            cancelAnimationFrame(move_down);
+            move_down = false;
         }
     }  
 });
@@ -51,6 +69,38 @@ function left(){
     }
 }
 
+function right(){
+    if (game_over === false && parseInt(my_car.css('left')) < gameWidth - my_car_width){
+        my_car.css('left', parseInt(my_car.css('left')) + 5);
+        move_right = requestAnimationFrame(right);
+    }
+}
+
+function up(){
+    if (game_over === false && parseInt(my_car.css('top')) > 0){
+        my_car.css('top', parseInt(my_car.css('top')) - 5);
+        move_up = requestAnimationFrame(up);
+    }
+}
+
+function down(){
+    if (game_over === false && parseInt(my_car.css('top')) < gameHeight - my_car_height){
+        my_car.css('top', parseInt(my_car.css('top')) + 5);
+        move_down = requestAnimationFrame(down);
+    }
+}
+
+
+function repeat(){
+    if (game_over === false){
+        car_down(car_1);
+        car_down(car_2);
+        car_down(car_3);
+    }
+}
+
+
+
 btnPlay.click(function () {
     btnPlay.fadeOut();
     processGame();
@@ -59,7 +109,9 @@ btnPlay.click(function () {
 
 function processGame() {
     moveRoad();
+    repeat();
     requestAnimationFrame(processGame);
+    requestAnimationFrame(repeat);
 }
 
 // function moveRoad() {
